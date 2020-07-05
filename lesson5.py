8d8aff1e1427b89c63c5aada4db21c94edb9199f
nick12321 <nick.thompson@rogers.com>
2020-07-04 21:08:33 -0400

    lesson5.py collision

--- lesson5.py
+++ lesson5.py
@@ -12,5 +12,25 @@ x=4
 
 if x == 5:
     print(" x = 5!")
-else:
-    print(" x is not 5!!")
+elif x < 5:
+    print(" x is less than 5!")
+elif x > 5:
+    print('x is greater than 5!')
+    
+# While loops
+
+x=0
+while x<=5:
+    print(x)
+    x+=1
+
+print('-----------')
+# same as for i in range(6):
+for i in range(1,5+1):
+    print(i)
+
+print('-----------')
+
+for i in range(5,-7,-2):
+    print(i)
+
