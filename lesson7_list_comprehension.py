448ca95cf70f53315fa259302ac6c1e3aef1d4c5
Nick Thompson <nick.thompson@rogers.com>
2020-08-23 18:41:17 -0400

    list comprehension

--- lesson7_list_comprehension.py
+++ lesson7_list_comprehension.py
@@ -56,6 +56,88 @@ print(list1)
 list2=[x.replace('\n',"") for x in list1]
 print(list2)
 
+#Iterator below does not work. Have asked Ed for clarification
+"""
+class Squares:
+    def __iter__(self):
+        self.x=1
+        return self
 
+def __next__(self):
+    if self.x<=10:
+        x=self.x
+        self.x+=1
+        return x*x
+    else:
+        raise StopIteration
+        
+itr=iter(Squares())
+for x in itr:
+    print(x)
+"""
+#simple Generator
+def simpleGenerator():
+    yield 1200
+    
+result=simpleGenerator()
 
+for x in result:
+    print(x)
 
+#Square generator
+def squareGenerator():
+    for x in range(10):
+        yield x*x
+
+result=squareGenerator()
+
+for x in result:
+    print(x)
+
+#Zip compress values together from seperate lists
+#uneven elements get dropped
+numbers=[1,2,3,4,5]
+letters=['a', 'b', 'c', 'Gerry', 'Tom', 'Karen']
+zipped=zip(numbers, letters)
+zipped=list(zipped)
+print(zipped)
+
+for x in zipped:
+    print(x)
+    
+list1=[i for i,j in zipped]
+list2=[j for i,j in zipped]
+print(list1)
+print(list2)
+
+print(*zipped)
+unzipped=zip(*zipped)
+unzipped=list(unzipped)
+print(unzipped)
+
+#Higher order functions
+list1=[1,2,3,4,5]
+
+def f(x):
+    return x + 1
+
+#map function returns an iterator of the function and the list
+itr = map(f, list1)
+list2=list(itr)
+print(list2)
+
+#same result as following for loop
+list1=[1,2,3,4,5]
+list2=[]
+for i in list1:
+    list2.append(f(i))
+print(list2)
+
+#To Do: make a square function and use map to square the numbers
+
+list1=[1,2,3,4,5,6,7,8,9]
+def f(x):
+    return x*x
+itr=map(f, list1)
+list2=list(itr)
+print(list2)
